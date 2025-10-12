a0d6ce0c221ff4ca1d8a2d47b596480853d0e481
rogod-gg <emredeneme31@outlook.com>
2025-10-12 09:55:31 +0300

    Update gg.lua

--- gg.lua
+++ gg.lua
@@ -1,9 +1,9 @@
 AGE_ACCS = {
-  --"emrekario7", "emrekario8", "gngrio1", "gngrio2", "gngrio3"
+  "emrekario7", "emrekario8", "gngrio1", "gngrio2", "gngrio3"
 }
 RIO_ACCS = {
-  --"emrekarseal", "emrekario", "emrekario2", "emrekario3", "emrekario4", "emrekario5"
+  "emrekarseal", "emrekario", "emrekario2", "emrekario3", "emrekario4", "emrekario5"
 }
 LITE_RIO_ACCS = {
-  "XxT0xicRiftxX2002", "gngrio3", "gngrio4","emrekario6", "emrekario7", "gngrio1", "gngrio2","emrekarseal", "emrekario", "emrekario2", "emrekario3", "emrekario4", "emrekario5"
+  "gngrio4"
 }
